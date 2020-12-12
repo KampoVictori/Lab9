4fe4d626a80d8cb355c076724de0c50de232c266
Victori <vika.kampo03@gmail.com>
2020-12-12 21:44:59 +0200

    first commit

---
+++ src/components/bookkeepingTemplate.vue
@@ -0,0 +1,27 @@
+<template>
+    <li>
+      <h3>ПІБ: {{ bk.nameW }}</h3>
+      <h3>Посада: {{ bk.post }}</h3>
+      <h3>З/п: {{ bk.salary }}</h3>
+      <h3>К-сть дітей: {{ bk.count_child }}</h3>
+      <h3>Стаж: {{ bk.experience }}</h3>
+    </li>
+</template>
+
+<script>
+export default {
+    name:"bookkeepingTemplate", 
+    props:{
+        bk: Object
+    },
+    data(){
+      return {
+
+      } 
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
